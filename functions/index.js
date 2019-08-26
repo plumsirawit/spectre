04b62bb35c963c20db633c371443a01fe7dbac95
Sirawit Pongnakintr <s6007589@mwit.ac.th>
2019-08-27 01:28:51 +0700

    Finished for today

--- functions/index.js
+++ functions/index.js
@@ -13,24 +13,43 @@ const cors = require('cors')({
 let db = admin.firestore();
 exports.registerUser = functions.region('asia-northeast1').https.onRequest((req, res) => {
   cors(req, res, () => {
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for(var i = 0; i < 16; i++){
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    if(!req.query.name){
-      res.send('No name found');
-    }else{
-      var name = req.query.name;
-      return db.collection('users').add({
-        uid: result,
-        name: name,
-        role: -1
-      }).then(ref => {
-        res.send(result);
+    db.collection('users').get().then((querySnapshot) => {
+      var cnt = 0;
+      querySnapshot.forEach((doc) => {
+        if(cnt != -1) cnt++;
+        if(doc.data().name == req.query.name){
+          cnt = -1;
+        }
       });
-    }
+      return cnt;
+    }).then((cnt) => {
+      if(cnt == 7){
+        return Promise.resolve('__error2');
+      }else if(cnt == -1){
+        return Promise.resolve('__error3');  
+      }else{
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for(var i = 0; i < 16; i++){
+          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        if(!req.query.name){
+          return Promise.resolve('__error1');
+        }else{
+          var name = req.query.name;
+          return db.collection('users').add({
+            uid: result,
+            name: name,
+            role: -1
+          }).then(ref => {
+            return result;
+          });
+        }
+      }
+    }).then((result) => {
+      res.send(result);
+    });
   });
 });
 
@@ -78,6 +97,38 @@ exports.resetUsers = functions.region('asia-northeast1').https.onRequest((req, r
   cors(req, res, () => {
     if(req.query.pass == 'shitshitshit'){
       return deleteCollection(db, 'users', 32).then(() => {
+        res.send('OK');
+      });
+    }else{
+      res.send('Unauthorized');
+    }
+  });
+});
+exports.startGame = functions.region('asia-northeast1').https.onRequest((req, res) => {
+  cors(req, res, () => {
+    if(req.query.pass == 'shitshitshit'){
+      // Random periods first, then 'select-permute' roles.
+      return db.collection('periods').get().then((querySnapshot) => {
+        var periods = [];
+        querySnapshot.forEach((doc) => {
+          periods.push(doc.data());
+        });
+        return Promise.resolve(periods);
+      }).then((periods) => {
+        var item = periods[Math.floor(Math.random()*periods.length)];
+        return db.collection('settings').doc('settings').update({currentPeriod: item.name});
+      }).then(() => {
+        return db.collection('users').get();
+      }).then((querySnapshot) => {
+        var shufArr = shuffle([0,1,2,3,4,5,6]);
+        var idx = 0;
+        var promises = [];
+        querySnapshot.forEach((doc) => {
+          promises.push(db.collection('users').doc(doc.id).update({role: shufArr[idx]}));
+          idx++;
+        });
+        return Promise.all(promises);
+      }).then(() => {
         res.send('OK');
       });
     }else{
